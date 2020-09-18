9a48e7538be3fbbce8e66a4c55440701ca937893
Wilson PLouis <wilsonpierrrlouis14@gmail.com>
2020-09-18 09:19:04 -0400

    Homework

---
+++ Homework 9.18.20.js
@@ -0,0 +1,69 @@
+// Q1: Write some code that will split up a string into a clear string names.
+    var str = 'Will+Smith'
+    var str = 'Michael+Jackson'
+    var str = 'Chris+Brown'
+    str.split('+');
+    console.log(str.split('+')) //
+
+// For example 'Manami+Anam' gets converted into 'Manami, Anam'.
+
+// For example 'Nic+Laurie' gets converted into 'Nic, Laurie'.
+
+ 
+// Q2: Write some code that gives us the length of a given string.
+    var string = 'Ice Cube';
+    var string = 'Lebron James';
+    var string = 'Michael B Jordan';
+    length = String.length();
+    console.log(string.length);
+
+// For example 'Peter Parker' gives us 12.
+
+// For example 'Michelle Obama' gives us 14.
+
+ 
+// Q3: a: Write some code that converts a string into all capitals
+    var str = 'Michael Jordan';
+    var str = 'Chris Tucker';
+    var str = 'Homer Simpson';
+    (str.toUpperCase());
+// For example: 'Peter Parker' changes to 'PETER PARKER'.
+
+// b: Write some code that converts a string into all lowercase.
+    var str = 'Michael Jordan';
+    var str = 'Chris Tucker';
+    var str = 'Homer Simpson';
+    (str.toLowerCase());
+
+// For example: 'Michelle Obama' converts 'michelle obama'.
+
+ 
+// Q4: a: Write some code that extrapolates the second word from the string: "My home town.".
+    var str = "My Favorite Show.";
+    var str = "Nothing To Do.";
+    str.split("My Show.");
+    str.split("Nothing Do.");
+// The result should be: 'home'
+
+// b: Write some code that extrapolates the second word from the string: "Lets eat, Grandpa!".
+var str = "2020 Is Terrible.";
+str.split("2020 Terrible.");
+// The result should be: 'eat'
+
+ 
+// Q5: Write some code that capitalizes a portion of a given string.
+    var day = "Today Was Terrible.";
+    day = day.substr(6,9).toUpperCase() + day.substr(9);
+// For example: "I'm very happy" becomes, "I'm VERY happy!"
+
+ 
+// Arrays:
+
+// Q6: Write some code that retrieves the 3rd item from the array ['Red', 'Green', 'Yellow', 'Purple']
+    var colors = ['Red', 'Green', 'Yellow', 'Purple'];
+    colors.split('Red', 'Green', 'Purple');
+ 
+
+// Q7: Write some code that converts the array ['Red', 'Green', 'Yellow', 'Purple'] to ['Green', 'Yellow', 'Blue', 'Purple', 'Pink']
+    var colors = ['Red', 'Green', 'Yellow', 'Purple'];
+    colors.splice(colors)
