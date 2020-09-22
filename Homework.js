ad55aa40e1f06f575b77db32c52d5ac312cd2c6b
Wilson PLouis <wilsonpierrrlouis14@gmail.com>
2020-09-22 00:55:55 -0400

    Homework

--- Homework.js
+++ Homework.js
@@ -2,26 +2,43 @@
 // Using Math.random(), console.log 3 random numbers from 5 to 15
 //function getRandomArbitrary(min, max){
  
-   // min = Math.ceil(min);
+   // min = Math.ceil(min);{
    // max = Math.floor(max);
- 
+    
    // return Math.floor(Math.random() * (max - min) + min)
- // }
+//}
 
-  //  console.log(getRandomArbitrary(5,15));
+   // console.log(getRandomArbitrary(5,15));
 
 // Problem 2
 // Define a variable name and assign your name to it
 // Create a function that returns your name
+  //var me = 'Will';
+  //function name(param){
+  //  return param;
+ // }
+
+ // console.log(name(me));
 
 // Problem 3
 // Create a function that accepts 2 arguments, both of which are numbers, and returns the sum of those two numbers
+//function add(num1, num2){
+ // var answer = num1 * num2
+ // return answer;
+//}
+//console.log(add(5, 25));
 
 // Problem 4
 // Create a function that returns the type of any argument that is being called in the function
 // example
 // type(400) // => 'number'
 // type('hi') // => 'string'
+//var guest = 'welcome to my website'
+ // function word(param){
+ // return param;
+//}
+
+  //console.log(word(guest));
 
 // Problem 5
 // Hint: Ensure you are modifying the parameter and not the original variable inside the function, this will make your life easier with this question
@@ -30,22 +47,66 @@
 // Using the array above, create a funciton that returns the 4th element in the array.
 
 // Create a function that returns only the last element in the fruits array
+//var fruits = ['apple', 'grape']
+  //function name(one){
+    //one.shift();
+   // return one
+ // }
+
+ // console.log(name(fruits));
 
 // Create a function that returns [peach', 'lemon', 'plum', 'grape']
+//var fruits = ['peach', 'lemon', 'plum', 'grape']
+ // function eat(more){
+  //  return more
+//  }
+
+ // console.log(eat(fruits));
 
 // Create a function that adds another furit to the end of the array and returns the new array
+//var fruits = ['kiwi', 'fig', 'blueberry']
+ // function eat(next){
+   // next.push('mango');
+  //  return next
+//  }
+
+ // console.log(eat(fruits));
 
 // Create a funciton that returns the string, 'apple and orange and peach and lemon and plum and grape and watermelon'
+//var fruits = apple and orange and peach and lemon and plum amd grape and watermelon
+//  function name(param){
+  //  return param;
+  //}
+
+  //console.log(name(fruits));
 
 // Problem 6
 // var names = ['Wil', 'Shamika', 'Samuel', 'Ramsey']
 
 // Using the array above, create a fucntion that returns the string, 'Hello Wil, happy Friday!'
+var wil = 'Hello Wil, happy Friday!'
+  function greet(param){
+    return param;
+  }
+
+  console.log(greet(wil));
 
 // Create a funciton that returns the string, 'Hi Shamika and Ramsey!'
+var name = 'Hi Shamika and Ramsey!'
+  function say(param){
+    return param
+  }
+
+  console.log(say(name));
 
 // Create a function that returns the string, 'Howdy Sam!'
 // Note: Be sure to change 'Samuel' to 'Sam'
+//var hey = 'Howdy Samuel!'
+  //function word(param){
+ //   return param
+ // }
+
+ // console.log(hey.slice(0,9);
 
 // Problem 7
 // var nums = [12, 3, 50, 1, 99, 2]
