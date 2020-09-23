b6205d387decac61cafe854d939a7eeac163a8c5
Wilson PLouis <wilsonpierrrlouis14@gmail.com>
2020-09-23 13:54:36 -0400

    Homework.js

--- Homework.js
+++ Homework.js
@@ -19,16 +19,17 @@
 //          - Loop Count: 2
 //          - Loop Count: 3
 //          - Etc.. based on the max value parameter
-  var num = [1,2,3];
-  function times(arr){
-    for(var i = 0; i < arr.length; i++){
-        var output = arr.indexOf(arr[i]);
-        console.log(output);
-       console.log(`Loop Count: ${output}`);
-    }
-    return
-}
-console.log(times(num))
+  //var num = [8,5,6];
+  //function times(arr){
+    //for(var i = 0; i < arr.length; i++){
+
+      //  var output = num.indexOf(arr[i]);
+       //console.log(Loop Count: ${i});
+    //}
+    //return
+//}
+//console.log(times(num))
+
 // Q3: Loop over the following array and console.log the current index value
 //      - ['Zeus', 'Hades', 'Hermies', 'Poseidon']
 //      - The console should similar to the following:
@@ -41,7 +42,7 @@ console.log(times(num))
       //      for(var i = 0; i < arr.length; i++){
         //        console.log(arr[i]);
           //  }
-            //return ''
+            //return
         //}
 
         //console.log(names(Power));
@@ -49,58 +50,103 @@ console.log(times(num))
 // Q4: Loop over an array of strings and return a new array of single letter strings
     // Input:    ['As', 'Soon', 'As' 'Possible']
     // Output: ['A','S','A','P']
-    var words = ['As', 'Soon', 'As', 'Possible'];
-        function names(arr){
-          var input = arr[i];
-          for(var i = 0; i < words.length;i++){
-            var input = words[i];
-            var mid = input.charAt(0)
-            output.push(mid);
-        }
+  //  var commad = ['As', 'Soon', 'As', 'Possible'];
 
-        console.log(names(words));
+    //    function word(arr){
+      //    var capWords = [];
+        //  for(var i = 0; i < words.length;i++){
+          //  var ele = arr[i];
+            //var first = ele[0]
+            // var capitalized = first
+            //capWords.push(capitalized)
+        //} 
+        //return capWords;
+    //}
+        //console.log(word(commad));
 
 // Q5: Loop over an array and log to the CLI an array where each word has its second letter capitalized
     // - Input:    ['Apple', 'Banana', 'Orange', 'Peach', 'Strawberry', 'Plum'];
     // - Output: ['APple', 'BAnana', 'ORange', 'PEach', 'STrawberry', 'PLum'];
-   // var arrOfStr = ['Apple', 'Banana', 'Orange', 'Peach', 'Strawberry', 'Plum'];
-    //function capscndLst(arr){
-      //  var capWords = [];
+    //var fruittree = ['Apple', 'Banana', 'Orange', 'Peach', 'Strawberry', 'Plum'];
+
+    //function capsec(arr){
+        //var capWords = [];
+
         //for(var i = 0; i < arr.length; i++){
-          //  var ele = arr[i]
-            //var one = elle.slice(0, 2).toUpperCase();
-            //var two = ele.slice(1, -2);
-            //var three = elle.slice(-2).toUpperCase();
-            //var four = elle.slice(-2).toUpperCase();
-            //var five = elle.slice(-2).toUpperCase();
-            //var six = elle.slice(-2).toUpperCase();
-            //var capitalized = one + two + three + four + five + six;
-        
-            //console.log(last)
+            //var ele = arr[i]
 
-          //  capWords.push(capitalized);
+          //var secondLet = ele[1].toUpperCase();
+          //var wordArr = ele.split('');
+          //wordArr[1] = secondLet;
+
+          //var capitalized = wordArr.join('');
+          //wordArr[1] = secondLet;
+
+          // var capitalized = wordArr.join('');
+          //capWords.push(capitalized);
         //}
-
-      //  return capWords;
-    //}
-
-   // console.log(capfrstLst(arrOfStr));
+        //return capWords;
+      //}
+     // console.log(capsec(fruittree))
 
 // Q6: Loop over an array of names and log the first and last initial of each person
     // - Input:    ['Shamika Earle', 'Ramsey Lewis', 'Samuel Pierre Louis', 'Wilson Pierre Louis' ]
     // - Output: ['SE', 'RL', 'SPL', 'WPL' ]
-    var names = ['Shamika Earle', 'Ramsey Lewis', 'Samuel Pierre Louis', 'Wilson Pierre Louis'];
-    function initial(arr){
-      for(var i = 0; i < arr.length; i++){
-                console.log(arr[i]);
-                var ele = arr[i]
-                name1.ele.splice(0,8).remove;
-                name2.ele.splice(16,23).remove;
-                name3.ele.splice(31,38,45).remove;
-                name4.ele.splice(53,60,67).remove;
-                var remover = name1 + name2 + name3 + name4;
-            }
-            return remover;
-        }
+    //var names = ['Shamika Earle', 'Ramsey Lewis', 'Samuel Pierre Louis', 'Wilson Pierre Louis'];
+    //function initial(arr){
+      //var output =[];
+      //for(var i = 0; i < arr.length; i++){
+          //var nameSplt = arr[i].split(' ');
+          //var initials = '';
+          //var spltName = nameSplt[0];
+          //var middle = spltName[0];
+          //var middle = initials;
 
-        console.log(initial(names));
+          //console.log(spltName);
+          //console.log(nameSplt);
+          //console.log(middle);
+
+          //for(var j = 0; j < nameSplt.length; j++){
+            //var currentName = nameSplt[j];
+            //var initial = currentName[0];
+            //initials = initials + initial;
+          //}
+          //output.push(initials);
+        //}
+      //  return output;
+    //}
+      //  console.log(initial(names));
+
+       // for Q5
+      //  var fruits = ['Apple', 'Banana', 'Orange', 'Peach', 'Strawberry', 'Plum'];
+        //function fruitCap(arr){
+          //for(var i = 0; i < arr.length; i++){
+            //var currentArr = arr[i];
+            //var upperCase = arr[i][1].toUpperCase();
+            //var endOfSting = currentArr.slice(2);
+            //var newString = arr[i][0] + upperCase + endOfSting;
+            //arr[i] = newString;
+          //}
+          //return arr;
+        //}
+        //console.log(fruitCap(fruits))
+
+  //Q6 Correct & Shorter
+  var names = ['Shamika Earle', 'Ramsey Lewis', 'Samuel Pierre Louis', 'Wilson Pierre Louis'];
+    function initial(arr){
+      var output = [];
+      
+      for(var i = 0; i < arr.length; i++){
+        var nameSplt = arr[i].split(' '); // ['Samuel', 'Pierre', 'Louis']
+        var initials = nameSplt[0][0]; // accumilator, starting with first initial, 'S'
+
+        for(var j = 1; j < nameSplt.length; j++){
+          var currentName = nameSplt[j]; // 'Pierre'
+          var initial = currentName[0]; // 'P'
+          initials = initials + initial; // 'S' + 'P'
+        }
+        output.push(initials);
+      }
+      return output;
+    }
+    console.log(initial(names))
