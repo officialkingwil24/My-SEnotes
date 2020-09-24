bda7e33390a3c09ff86a564f0dabb0192b382e07
Wilson PLouis <wilsonpierrrlouis14@gmail.com>
2020-09-24 12:19:17 -0400

    ColdWarsPractice.js

--- Homework.js
+++ Homework.js
@@ -1,18 +1,6 @@
-var key = 'Name';
-        var obj1 = {
-            name: 'Ice Cube',
-            skin: 'Black',
-            hairColor: 'Black',
-            eyeColor: 'Dark Brown',
-            height: "5'8",
-            age: '51',
-            favorite: ['It Was a Good Day']
-        }
-        function searchKey(name, num){
-            if (name[num]){
-                return name[num]
-            } else {
-                return 'No such value'
-            }
-        }
-        console.log(searchKey(obj1, 'favorite'))
+//Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+function even_or_odd(number) {
+    console.log(number)
+    return number
+}
+console.log(even_or_odd(8));
