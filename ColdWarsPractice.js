137c09a2d80f596a96373f9cd4d4f92c941aa2e8
Wilson PLouis <wilsonpierrrlouis14@gmail.com>
2020-09-25 14:32:07 -0400

    ColdWarsPractice.js

--- ColdWarsPractice.js
+++ ColdWarsPractice.js
@@ -66,3 +66,97 @@
 //     }
 // }
 // console.log(makeNegative(10))
+
+//Write a function called repeat_str which repeats the given string src exactly count times.
+//repeatStr(6, "I") // "IIIIII"
+//repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+//function repeatStr (n, s) {
+  //return ''
+//}
+//var cash = 'money'
+//function repeatStr (n, Money) {
+  //return Money.repeat(n);
+//}
+//console.log(cash.repeat(5))
+
+//We need a function that can transform a number into a string.
+//What ways of achieving this do you know?
+//Examples:
+//numberToString(123); // returns '123';`   
+//numberToString(999); // returns '999';`
+//solution 1
+//function numberToString(n) {
+  // return n.toString()
+//}
+//console.log(numberToString(666999))
+//solution 2
+// function numberToString(num) {
+//   return String(num);
+// }
+// console.log(numberToString(666999))
+//solution 3
+// function numberToString(num) {
+//   return ''+num;
+// }
+// console.log(numberToString(666999))
+
+//Complete the solution so that it reverses the string passed into it.
+//'world'  =>  'dlrow'
+//function solution(str){
+  
+//}
+
+//function reverseString(str) {
+  //for(var i = 0; i < arr.length; i++){
+
+  //}
+  //return str
+//}
+//console.log(reverseString('evil'));
+
+// function reverse(str){
+//   let reversed = "evil";    
+//   for (var i = str.length - 1; i >= 0; i--){        
+//     reversed += str[i];
+//   }    
+//   return reversed
+// }
+// console.log(reverse(evil))
+
+var arr = 'evil'
+function reverseString(str){
+  var reversed = []
+  for (var i = str.length - 1; i >=0 ; i--) {
+    reversed.push(str[i])
+  }
+  return reversed.join('')
+}
+console.log(reverseString(arr))
+
+//Given an array of integers your solution should find the smallest integer.
+//For example:
+//Given [34, 15, 88, 2] your solution will return 2
+//Given [34, -345, -1, 100] your solution will return -345
+//class SmallestIntegerFinder {
+  //findSmallestInt(args) {
+    
+  //}
+//}
+function findSmallestInt(args){
+  let min = Math.min.apply(null, args);
+  return min;
+}
+
+function findSmallestInt(args){
+  //create a variable that holds the smallest integer, initially fil it with the first elemnt
+  var smallestInt = args[0];
+  // loop through all our array elements (starting at 1 for effiency)
+  for(var i = 1; i < args.length; i++){
+    if(args[i] < smallestInt){
+      smallestInt = args[i];
+    }
+  }
+  return smallestInt;
+}
+console.log(findSmallestInt([1,2,3,4,5,-1,-100]))
+console.log(findSmallestInt([1,2,3,4,5,0]))
