81c5ee4783541804855613a6294051a7b14035c9
Wilson PLouis <wilsonpierrrlouis14@gmail.com>
2020-09-25 14:34:24 -0400

    HigherOrderNotes.js

---
+++ HigherOrderNotes.js
@@ -0,0 +1,50 @@
+// Higher order functions
+        // .filter()
+        // .reduce()      
+        // .map()
+  
+    
+
+// filter questions
+// Level 0
+    // given an array of numbers filter out any that are larger than 10
+
+// Level 1
+    // given an array of numbers filter out any that are divisible by 3
+
+    // given an array of nubers filter out any that are even
+
+// Level 2
+    // given an array of strings, filter out any that are longer than 4 chars
+
+    // given an array of arrays, filter out any that are shorter than 10 elements
+
+// Level 3
+    // given an array of objects, filter out any that dont have a key called 'firstName'
+
+// reduce questions 
+// level 0:
+    // given an array of numbers add them all together
+
+// level 1:
+    // given an array of numbers, multiply them all together
+
+// level 2:
+    // given an array of strings, concatiante them together 
+
+// level 3: 
+    // given an array of arrays make them all into one array 
+
+// level 5:
+    // given an array of strings, remove any vowels and concatenate them toegher 
+
+
+// map questions
+// level 0:
+    // given an array of numbers, return a new array with all numbers multiplied by 2
+
+// level 1:
+    // given an array of numbers, return a new array with all numbers increased by 1
+
+// level 2:
+    // given an array of strings, return a new array with all strings as capitals
