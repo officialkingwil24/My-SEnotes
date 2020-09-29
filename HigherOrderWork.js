8b206b18d295f1a0ab7951b8bf5d15c6e400cd4c
Wilson PLouis <wilsonpierrrlouis14@gmail.com>
2020-09-29 12:05:58 -0400

    HigherOrderWork.js

---
+++ HigherOrderWork.js
@@ -0,0 +1,22 @@
+// Higher order exercises pt 2. No given Higher order function to use
+    
+
+// given an array of people objects, return only the females
+    // the object will look like this: let sally = {name: 'Sally', age:55, gender: 'female'}
+
+
+// given an array of numbers, return an array without any even numbers
+
+
+// given an array of people objects, return an array with all ages
+    // the object will look like this: let sally = {name: 'Sally', age:55}
+    // part 2 find the average age of all people in the array (run a higher order function on your new array)
+
+
+// given an array of arrays, return an array of just the first elements of those inner arrays (only one array deep)
+
+
+// given an array of full names ('Sally Wadsworth') return the first and last initials
+
+
+// given an array of numbers find the largest number 
