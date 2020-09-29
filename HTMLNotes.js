d15fd289aa4ea66da49a56f34b173d72a450f5bd
Wilson PLouis <wilsonpierrrlouis14@gmail.com>
2020-09-29 16:08:28 -0400

    index.html

---
+++ HTMLNotes.js
@@ -0,0 +1,26 @@
+// ### What is HTML?
+//is the standard markup language for creating Web pages. HTML describes the structure of a Web page. HTML consists of a series of elements. HTML elements tell the browser how to display the content.
+
+// ### How does it relate to the internet?
+//HTML is used to create electronic documents (called pages) that are displayed on the World Wide Web. Each page contains a series of connections to other pages called hyperlinks. Every web page you see on the Internet is written using one version of HTML code or another
+
+// ### How does it relate to the "web development stack"
+//is a programming language used to describe the structure of information on a web page. Together, HTML, CSS, and JavaScript make up the essential building blocks of websites, with CSS controlling a page's appearance, and JavaScript programming its functionality
+
+// ### How does it relate to JavaScript?
+//html tags create objects; JavaScript lets you manipulate those objects.
+
+// ### What are the parts of HTML?
+//HTML have 3 parts 
+//a line containing HTML version information,
+//a declarative header section (delimited by the HEAD element),
+//a body, which contains the document's actual content. The body may be implemented by the BODY element or the FRAMESET element.
+
+// ### What are HTML Tags?
+//HTML tags begin with the less-than (<) character and end with greater-than (>). These symbols are also called "angle brackets."
+
+// ### What are some commonly used HTML elements?
+//is head tag, html tag, body tag, title tag, heading tag, Paragraph tag, Emphasis tag
+
+// ### What are HTML attributes?
+//HTML attributes are special words used inside the opening tag to control the element's behaviour. HTML attributes are a modifier of an HTML element type.
--- HigherOrderWork.js
+++ HigherOrderWork.js
@@ -1,17 +1,19 @@
 // Higher order exercises pt 2. No given Higher order function to use
-    
-
 // given an array of people objects, return only the females
     // the object will look like this: let sally = {name: 'Sally', age:55, gender: 'female'}
-
+    //let arrOfObj1 = [{name:'jack', age:'39', gender:'male'}, {name:'June', age:'21', gender:'female'}, {name:'Love', age:'25', gender:'female'}]
+    //let women = arrOfObj1.filter(function(obj){return obj['gender'] === 'female'});
+     //console.log(women);
 
 // given an array of numbers, return an array without any even numbers
-
+let numbers = ['2', '3', '4', '5', '6', '7'];
+    let evenNums = numbers.filter(function(n){return n %2 === 1})
+    console.log(evenNums);
 
 // given an array of people objects, return an array with all ages
     // the object will look like this: let sally = {name: 'Sally', age:55}
     // part 2 find the average age of all people in the array (run a higher order function on your new array)
-
+let people = [{name:'Mack', age:'90'}, {name:'Fred', age:'41'}, {name:'Shy', age:'32'}, {name:'Jason', age:'27'}]
 
 // given an array of arrays, return an array of just the first elements of those inner arrays (only one array deep)
 
