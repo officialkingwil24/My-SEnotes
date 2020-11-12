ce6812eb326530b2ec1f4c06703732582800e1f2
Wilson PLouis <wilsonpierrrlouis14@gmail.com>
2020-11-12 09:28:23 -0500

    added

--- PSQL_HW/queries.js
+++ PSQL_HW/queries.js
@@ -1,73 +1,44 @@
-const Pool = require('pg').Pool
+const Pool = require('pg').Pool;
 const pool = new Pool({
-  user: 'me',
-  host: 'localhost',
-  database: 'api',
-  password: 'postgres',
-  port: 5432,
+    user: 'postgres',
+    host: 'localhost',
+    database: 'defjam',
+    password: 'postgres',
+    port: 5432,
 })
-const psql = (request, response) => {
-  pool.query('SELECT * FROM psql ORDER BY id ASC', (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
+
+const getStreetfighter = (request, response) => {
+    pool.query('SELECT * FROM streetfighter', (error, results) => {
+        if(error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
 }
 
-const getPsqlById = (request, response) => {
-  const id = parseInt(request.params.id)
-
-  pool.query('SELECT * FROM psql WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).json(results.rows)
-  })
+const getStreetfighterById = (request, response) => {
+    const id = parseInt(request.params.id);   
+    pool.query('SELECT * FROM streetfighter WHERE id = $1, [id]', (error, results) =>{
+        if(error){
+            throw error;
+        }
+        response.status(200).json(results.rows)
+    });
 }
 
-const createPsql = (request, response) => {
-  const { name, email } = request.body
+const createStreetfighter = (request, response) => {
+    const {name, nickname, power, health, mobility, techniques, range, occupation, style, skill} = request.body;
 
-  pool.query('INSERT INTO psql (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(201).send(`Psql added with ID: ${result.insertId}`)
-  })
-}
-
-const updatePsql = (request, response) => {
-  const id = parseInt(request.params.id)
-  const { name, email } = request.body
-
-  pool.query(
-    'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-    [name, email, id],
-    (error, results) => {
-      if (error) {
-        throw error
-      }
-      response.status(200).send(`Psql modified with ID: ${id}`)
-    }
-  )
-}
-
-const deletePsql = (request, response) => {
-  const id = parseInt(request.params.id)
-
-  pool.query('DELETE FROM psql WHERE id = $1', [id], (error, results) => {
-    if (error) {
-      throw error
-    }
-    response.status(200).send(`Psql deleted with ID: ${id}`)
-  })
+    pool.query('INSERT INTO Streetfighter (name, skill) VALUES ($1, $2) RETURNING *', [name, skill], (error, results) =>{
+        if(error){
+            throw error;
+        }
+        response.status(200).send(`Streetfighter added with name: ${name}, skill: ${skill}`);
+    })
 }
 
 module.exports = {
-  getPsql,
-  getPsqlById,
-  createPsql,
-  updatePsql,
-  deletePsql,
+    getStreetfighter,
+    getStreetfighterById,
+    createStreetfighter
 }
--- PSQL_HW/scripts.js
+++ PSQL_HW/scripts.js
@@ -1,42 +1,44 @@
-let allDefjam = document.getElementById('allDefjam');
-function addDefjam(e){
-    let defjam = {
+let allStreetfighter = document.getElementById('allStreetfighter');
+function addStreetfighter(e){
+    let streetfighter = {
     name: document.getElementById('name').Value,
-    image: document.getElementById('image').Value,
+    nickname: document.getElementById('nickname').Value,
+    power: document.getElementById('power').Value,
     health: document.getElementById('health').Value,
-    speed: document.getElementById('speed').Value,
-    tough: document.getElementById('tough').Value,
-    skill: document.getElementById('skill').Value,
-    attackname: document.getElementById('attackname').Value,
-    moves: document.getElementById('moves').Value,
+    mobility: document.getElementById('mobility').Value,
+    techniques: document.getElementById('techniques').Value,
+    range: document.getElementById('range').Value,
+    occupation: document.getElementById('occupation').Value,
+    style: document.getElementById('style').Value,
+    skill: document.getElementById('skill').Value
     }
-    fetch('http://localhost:3000/defjam', {
+    fetch('http://localhost:3000/streetfighter', {
         method: 'POST',
         headers: {
             'Content-Type': 'applcation/json'
         },
-        body: JSON.stringify(defjam)
+        body: JSON.stringify(streetfighter)
     })
     .then(res => res.json())
     .catch(error => console.log(error))
 
-    getDefjam();
+    getStreetfighter();
 }
 
-function getDefjam(){
+function getStreetfighter(){
     allDefjam.innerHTML = '';
-    fetch('http://localhost:3000/character', {method: 'GET'})
+    fetch('http://localhost:3000/streetfighter', {method: 'GET'})
     .then(res => res.json())
     .then(res => {
-        res.forEach(defjam => {
+        res.forEach(streetfighter => {
             let card = document.createElement('div');
 
             card.setAttribute('class', 'card');
 
-            card.innerText = defjam.name;
+            card.innerText = streetfighter.name;
 
-            allDefjam.appendChild(card)
-            console.log(defjam)
+            allStreetfighter.appendChild(card)
+            console.log(Streetfighter)
         });
     })
     .catch(error => console.log(error))
